cd94977d235b761bfcec3cffce6310204ad606f0
muath-ye <34031333+muath-ye@users.noreply.github.com>
2024-09-16 11:06:48 +0300

    feat: add offline

--- sw.js
+++ sw.js
@@ -1,23 +1,37 @@
 const CACHE_NAME = 'qr-code-pwa-v1';
 const assetsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    'https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/qrcode-generator/1.4.4/qrcode.min.js'
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './tailwind.min.css',
+    './qrcode.min.js'
 ];
+
 
 self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(assetsToCache))
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets);
+        })
     );
 });
 
 self.addEventListener('fetch', event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys => {
+            return Promise.all(
+                keys.filter(key => key !== cacheName)
+                    .map(key => caches.delete(key))
+            );
+        })
     );
 });
